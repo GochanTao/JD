4d215f7d4f62ba4a71be1818be83d48dc4d6f27f
Taoguoqiang <1604002085@qq.com>
2017-10-09 17:21:49 +0800

    add return top fun

--- JD/js/index_js.js
+++ JD/js/index_js.js
@@ -1,90 +1,105 @@
 /* Auther: Gochan Tao
 time: 2017/10/6 - 2017/10/8 */
 
-$(document).ready(function (){
-      /*关闭头部广告栏*/
-      $("#topbanner i").click(function (){
-      	$("#topbanner").css("display","none");
-      })
+ $(document).ready(function (){
+       /*关闭头部广告栏*/
 
-      function dropdown (selector,child){
-      	$(selector).hover(function (){
-      	$(this).find(child).show();
-      },function (){
-      	$(this).find(child).hide();
-      })
-      }
-      dropdown("#shortcut .dt",".province");
-      dropdown("#shortcut .fore",".dt");
-      dropdown("#shortcut .service",".dt");
-      dropdown("#shortcut .net-nav",".dt");
-      dropdown("#adpart .leftpart li",".dt");
+       $("#topbanner i").click(function (){
+       	$("#topbanner").css("display","none");
+       })
 
-      /*轮播图*/
-      	var _index=null;
-      $("#adpart .flash .indicator i").hover(function()
-		{
-			clearInterval(timer);
-			 _index=$(this).index();
-			$(this).addClass("active").siblings("i").removeClass("active");
-			$("#adpart .flash>ul").animate({top:-(_index*340)},500);
-		},function(){autoplay()});
+       function dropdown (selector,child){
+       	$(selector).hover(function (){
+       	$(this).find(child).show();
+       },function (){
+       	$(this).find(child).hide();
+       })
+       }
+       dropdown("#shortcut .dt",".province");
+       dropdown("#shortcut .fore",".dt");
+       dropdown("#shortcut .service",".dt");
+       dropdown("#shortcut .net-nav",".dt");
+       dropdown("#adpart .leftpart li",".dt");
 
-		function autoplay()
-		{
-			timer=setInterval(function(){
-			_index++;
-			if(_index>7)
-				{
-					_index=0;
-					$("#adpart .flash>ul").css("top","0");
-				}
+       /*轮播图*/
+       	var _index=null;
+       $("#adpart .flash .indicator i").hover(function()
+ 		{
+ 			clearInterval(timer);
+ 			 _index=$(this).index();
+ 			$(this).addClass("active").siblings("i").removeClass("active");
+ 			$("#adpart .flash>ul").animate({top:-(_index*340)},500);
+ 		},function(){autoplay()});
 
-				$("#adpart .flash .indicator i").eq(_index).addClass("active").siblings("i").removeClass("active");
-				$("#adpart .flash>ul").animate({top:-(_index*340)},500);
+ 		function autoplay()
+ 		{
+ 			timer=setInterval(function(){
+ 			_index++;
+ 			if(_index>7)
+ 				{
+ 					_index=0;
+ 					$("#adpart .flash>ul").css("top","0");
+ 				}
+
+ 				$("#adpart .flash .indicator i").eq(_index).addClass("active").siblings("i").removeClass("active");
+ 				$("#adpart .flash>ul").animate({top:-(_index*340)},500);
 			
 				
-			},4000);
-		};
-		autoplay();
+ 			},4000);
+ 		};
+ 		autoplay();
 
 
-		/*右侧选项卡*/
-		$("#adpart .rightpart .news .tab-head .left-item").hover(function (){
-			$(this).addClass("active").siblings(".left-item").removeClass("active");
-			var _index =$(this).index(".left-item");
-			$("#adpart .rightpart .news .content").eq(_index).show().siblings().hide();
-		},function (){})
+ 		/*右侧选项卡*/
+ 		$("#adpart .rightpart .news .tab-head .left-item").hover(function (){
+ 			$(this).addClass("active").siblings(".left-item").removeClass("active");
+ 			var _index =$(this).index(".left-item");
+ 			$("#adpart .rightpart .news .content").eq(_index).show().siblings().hide();
+ 		},function (){})
 
 
-		/*轮播图按钮*/
-		$("#adpart .ad .flash").hover(function (){
-			$("#adpart .ad .flash a").show();
-		},function (){
-			$("#adpart .ad .flash .btn").hide();
-		})
+ 		/*轮播图按钮*/
+ 		$("#adpart .ad .flash").hover(function (){
+ 			$("#adpart .ad .flash a").show();
+ 		},function (){
+ 			$("#adpart .ad .flash .btn").hide();
+ 		})
 
-		$("#adpart .ad .flash .left-btn").click(function (){
+ 		$("#adpart .ad .flash .left-btn").click(function (){
 
-			_index--;
-			if(_index<0)
-				{
-					_index=7;
-					// $("#adpart .flash>ul").css("top","0");
-				}
-			$("#adpart .flash .indicator i").eq(_index).addClass("active").siblings("i").removeClass("active");
-			$("#adpart .flash>ul").animate({top:-(_index*340)},500);
-		})
-		$("#adpart .ad .flash .right-btn").click(function (){
+ 			_index--;
+ 			if(_index<0)
+ 				{
+ 					_index=7;
+ 					// $("#adpart .flash>ul").css("top","0");
+ 				}
+ 			$("#adpart .flash .indicator i").eq(_index).addClass("active").siblings("i").removeClass("active");
+ 			$("#adpart .flash>ul").animate({top:-(_index*340)},500);
+ 		})
+ 		$("#adpart .ad .flash .right-btn").click(function (){
 
-			_index++;
-			if(_index>7)
-				{
-					_index=0;
-					 $("#adpart .flash>ul").css("top","0");
-				}
-			$("#adpart .flash .indicator i").eq(_index).addClass("active").siblings("i").removeClass("active");
-			$("#adpart .flash>ul").animate({top:-(_index*340)},500);
-		})
-    })
-    
+ 			_index++;
+ 			if(_index>7)
+ 				{
+ 					_index=0;
+ 					 $("#adpart .flash>ul").css("top","0");
+ 				}
+ 			$("#adpart .flash .indicator i").eq(_index).addClass("active").siblings("i").removeClass("active");
+ 			$("#adpart .flash>ul").animate({top:-(_index*340)},500);
+	     })
+	  
+	  			/*回到顶部*/
+
+			$("#fixbar-bottom .icon-arrow-up").click(function (){
+				
+				$("html,body").animate(
+					{"scrollTop": 0
+
+					},1000);
+			});
+
+
+
+ 		})
+
+
