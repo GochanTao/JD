83c637359284248c32c4fe86abf283348b084fa0
Taoguoqiang <1604002085@qq.com>
2017-10-14 14:30:40 +0800

    delate yuan

--- JD/js/yuan_js.js
+++
@@ -1,9 +0,0 @@
- var toTop = document.getElementById("fixbar-bottom").getElementsByTagName("li")[0];
-    console.log(toTop);
-    toTop.oncick=function (){
-    	var scrollTop=window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    	console.log(scrollTop);
-    	if(scrollTop>0){
-    		scrollTop=0;
-    	}
-    }
